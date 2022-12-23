e261a96ebb1889e75c927535aa37aac85b840d2b
mlange-42 <martin_lange_@gmx.net>
2022-12-23 17:52:25 +0100

    print everything that is not a list or report to stderr

--- out/out.go
+++ out/out.go
@@ -2,6 +2,7 @@ package out
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/gookit/color"
 )
@@ -14,30 +15,34 @@ var (
 
 // Print prints a neutral message
 func Print(format string, a ...interface{}) {
-	print(format, a...)
+	printOut(format, a...)
 }
 
 // Err prints an error
 func Err(format string, a ...interface{}) {
 	errorColor.Print(" ERROR ")
 	fmt.Print(" ")
-	print(format, a...)
+	printErr(format, a...)
 }
 
 // Warn prints a warning message
 func Warn(format string, a ...interface{}) {
 	warningColor.Print(" WARNING ")
 	fmt.Print(" ")
-	print(format, a...)
+	printErr(format, a...)
 }
 
 // Success prints a success message
 func Success(format string, a ...interface{}) {
 	successColor.Print(" SUCCESS ")
 	fmt.Print(" ")
-	print(format, a...)
+	printErr(format, a...)
 }
 
-func print(format string, a ...interface{}) {
+func printOut(format string, a ...interface{}) {
 	fmt.Printf(format, a...)
+}
+
+func printErr(format string, a ...interface{}) {
+	fmt.Fprintf(os.Stderr, format, a...)
 }
