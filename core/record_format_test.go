e7f5d24ba20bc8e09d44d0fa19a537886d473dac
mlange-42 <martin_lange_@gmx.net>
2023-01-07 01:20:26 +0100

    add benchmarks for reading large datasets

--- core/record_format_test.go
+++ core/record_format_test.go
@@ -1,6 +1,10 @@
 package core
 
 import (
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"strings"
 	"testing"
 	"time"
 
@@ -113,6 +117,48 @@ func BenchmarkDeserialize(b *testing.B) {
 	}
 }
 
+func BenchmarkRead1k(b *testing.B) {
+	dir, err := ioutil.TempDir("", "track-test")
+	assert.Nil(b, err, "Error creating temporary directory")
+	defer os.Remove(dir)
+
+	track, err := NewTrack(&dir)
+	assert.Nil(b, err, "Error creating Track instance")
+
+	generateDataset(
+		&track,
+		util.Date(1990, 1, 1),
+		2*time.Hour,
+		1000,
+	)
+
+	for i := 0; i < b.N; i++ {
+		_, err = track.LoadAllRecords()
+		assert.Nil(b, err, "error loading records")
+	}
+}
+
+func BenchmarkRead10k(b *testing.B) {
+	dir, err := ioutil.TempDir("", "track-test")
+	assert.Nil(b, err, "Error creating temporary directory")
+	defer os.Remove(dir)
+
+	track, err := NewTrack(&dir)
+	assert.Nil(b, err, "Error creating Track instance")
+
+	generateDataset(
+		&track,
+		util.Date(1990, 1, 1),
+		2*time.Hour,
+		10000,
+	)
+
+	for i := 0; i < b.N; i++ {
+		_, err = track.LoadAllRecords()
+		assert.Nil(b, err, "error loading records")
+	}
+}
+
 func fullRecord() Record {
 	return Record{
 		Project: "test",
@@ -132,5 +178,69 @@ func fullRecord() Record {
 		},
 		Note: "Note with a +tag",
 		Tags: []string{"tag"},
+	}
+}
+
+func generateDataset(t *Track, start time.Time, step time.Duration, records int) error {
+	currTime := start
+	for i := 0; i < records; i++ {
+		rec := timedRecord(currTime, step/2, rand.Intn(3), rand.Intn(3))
+		if err := t.SaveRecord(&rec, false); err != nil {
+			return err
+		}
+		currTime = currTime.Add(step)
+	}
+	return nil
+}
+
+var noteLines = strings.Split(
+	`Lorem ipsum dolor sit +amet, consectetur adipiscing elit,
+sed do eiusmod tempor incididunt ut +labore et dolore magna aliqua.
+A diam sollicitudin +tempor id eu nisl nunc mi ipsum.
+Ullamcorper sit amet +risus +nullam eget felis eget.
+Cursus euismod quis viverra nibh. Nunc sed blandit libero volutpat sed.
+At augue eget arcu dictum varius +duis at consectetur lorem.
+Velit euismod in pellentesque +massa placerat duis ultricies.
+Risus nec +feugiat in fermentum posuere urna.
+Id diam vel quam +elementum +pulvinar +etiam non quam.
+Adipiscing bibendum est ultricies +integer quis auctor elit sed.
+Sagittis orci a scelerisque purus +semper eget duis.
+Elementum tempus egestas sed sed risus pretium.
+Sit amet +mattis vulputate enim nulla aliquet porttitor lacus luctus.
+Dignissim +sodales ut eu sem integer vitae justo eget.
+Posuere urna nec tincidunt +praesent semper feugiat nibh sed pulvinar.
+Dignissim cras tincidunt lobortis +feugiat vivamus at augue eget.
+Viverra nam libero justo laoreet sit +amet cursus.
+Venenatis +lectus +magna +fringilla +urna porttitor rhoncus dolor.
+Aliquam id +diam maecenas ultricies.`, "\n")
+
+func timedRecord(start time.Time, duration time.Duration, pauses int, lines int) Record {
+	pause := make([]Pause, pauses, pauses)
+	note := make([]string, lines, lines)
+
+	if pauses > 0 {
+		step := time.Duration(int(duration) / (pauses + 2))
+		pauseStart := start.Add(step / 2)
+		for i := 0; i < pauses; i++ {
+			pause[i] = Pause{
+				Start: pauseStart,
+				End:   pauseStart.Add(step / 2),
+				Note:  "Pause comment",
+			}
+			pauseStart = pauseStart.Add(step)
+		}
+	}
+
+	for i := 0; i < lines; i++ {
+		note[i] = noteLines[rand.Intn(len(noteLines))]
+	}
+
+	return Record{
+		Project: "test",
+		Start:   start,
+		End:     start.Add(duration),
+		Pause:   pause,
+		Note:    strings.Join(note, "\n"),
+		Tags:    []string{},
 	}
 }
